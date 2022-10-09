dcb993e9acdd8c150c3f6feb925f15dbb8bbbb87
EC2 Default User <ec2-user@ip-172-31-47-37.ec2.internal>
2022-10-09 21:37:45 +0000

    Finish feat/devise_japanese

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  validates :name, presence: true
 end
